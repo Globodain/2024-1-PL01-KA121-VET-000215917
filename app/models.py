9b4825488f2edf61aab2fee6a272683c6d40c295
Hubert Kosiaty <hubert.kosiaty@gmail.com>
2025-02-12 12:41:34 +0100

    more info in profile and edit option

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
@@ -14,6 +15,11 @@ class User(UserMixin, db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+                                             default=lambda: datetime.now(timezone.utc))
+    
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
 
@@ -25,6 +31,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)

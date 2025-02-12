629b833ba1b145f163685b004b6b6c95f51f3d43
Krzysztof Padlo <padlo.krzysiek@gmail.com>
2025-02-12 12:56:29 +0100

    feat: Added registration view and endpoint. chore: Created .gitignore file.

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from urllib.parse import urlsplit
 from app import app, db
 from app.models import User
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 
@@ -25,6 +25,23 @@ def index():
     ]
     return render_template('index.html', title='Home Page', posts=posts)
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():

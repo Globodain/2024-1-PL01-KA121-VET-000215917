086dc3211e1d910fab489b9e0fad6682d36522cd
Kacper Miterka <kacpermiterka06@gmail.com>
2025-02-13 12:57:21 +0100

    Page Navigation

--- app/routes.py
+++ app/routes.py
@@ -12,22 +12,6 @@ from urllib.parse import urlsplit
 from datetime import datetime, timezone
 from app.forms import EmptyForm
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -151,6 +135,22 @@ def index():
         db.session.commit()
         flash('Your post is now live')
         return redirect(url_for('index'))
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template("index.html", title='Home Page', form=form,posts=posts)
-   
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,posts=posts.items, next_url=next_url,prev_url=prev_url)
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,next_url=next_url, prev_url=prev_url)

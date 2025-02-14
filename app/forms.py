36c06a0af21d6abb7624563edd35094a6bdf4a6e
Kacper Miterka <kacpermiterka06@gmail.com>
2025-02-14 10:03:48 +0100

    Resetting a User Password

--- app/forms.py
+++ app/forms.py
@@ -53,3 +53,9 @@ class PostForm(FlaskForm):
 class ResetPasswordRequestFrom(FlaskForm):
     email= StringField('Email', validators=[DataRequired(),Email()])
     submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')

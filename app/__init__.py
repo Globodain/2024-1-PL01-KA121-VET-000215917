082ed58e8c29d320a00038abd21691f2f541bca0
Bartosz.Malec <bartekkulik1987@gmail.com>
2025-02-14 12:58:43 +0100

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask import request
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -7,6 +8,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
+from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +21,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:

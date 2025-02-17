58061fa1e79e58084749638e589010b3c4c1b6ec
Hubert Kosiaty <hubert.kosiaty@gmail.com>
2025-02-17 12:06:58 +0100

    added api

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,9 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -34,7 +37,7 @@ if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                       backupCount=10)
+                                    backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)

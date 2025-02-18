a3308cb7a529736787c4d39a9e57553e6085c750
Krzysztof Padlo <padlo.krzysiek@gmail.com>
2025-02-18 09:35:14 +0100

    feat: Connection with database

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
+import pymongo
 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["cars"]
+col = db["cars"]
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():

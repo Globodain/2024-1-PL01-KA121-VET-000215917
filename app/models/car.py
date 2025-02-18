4690234eb1ba68d1e61d5d09b03583fcabd3bd3b
Krzysztof Padlo <padlo.krzysiek@gmail.com>
2025-02-18 11:28:04 +0100

    feat: Enhance Car model with validation and add API endpoint for retrieving cars by brand

--- app/models/car.py
+++ app/models/car.py
@@ -1,32 +1,30 @@
 from pydantic import BaseModel, Field
-from models.range import Range
 
 
 class Car(BaseModel):
     brand: str
     model: str
-    year: int
-    milage: int
-    avg_price: int
-    gears: int
-    range: Range
-    seats: int
-    doors: int
-    trunk_capacity: float
-    max_speed: int
-    acceleration: float
+    year: int = Field(..., ge=1900)
+    milage: int = Field(..., ge=0)
+    avg_price: int = Field(..., ge=0)
+    gears: int = Field(..., ge=1)
+    seats: int = Field(..., ge=1)
+    doors: int = Field(..., ge=1)
+    trunk_capacity: float = Field(..., ge=0)
+    max_speed: int = Field(..., ge=0)
+    acceleration: float = Field(..., ge=0)
 
-    horse_power: int
-    displacement: float
-    cylinders: int
-    fuel_type: str
-    transmission: str
+    horse_power: int = Field(..., ge=0)
+    displacement: float = Field(..., ge=0)
+    cylinders: int = Field(..., ge=0)
+    fuel_type: str = Field(..., min_length=3)
+    transmission: str = Field(..., min_length=3)
 
-    fuel_consumption_city: float
-    fuel_consumption_highway: float
-    fuel_consumption_combined: float
-    fuel_tank_capacity: float
+    fuel_consumption_city: float = Field(..., ge=0)
+    fuel_consumption_highway: float = Field(..., ge=0)
+    fuel_consumption_combined: float = Field(..., ge=0)
+    fuel_tank_capacity: float = Field(..., ge=0)
 
-    range_city: float
-    range_highway: float
-    range_combined: float
+    range_city: float = Field(..., ge=0)
+    range_highway: float = Field(..., ge=0)
+    range_combined: float = Field(..., ge=0)
--- app/routes/cars.py
+++ app/routes/cars.py
@@ -9,7 +9,19 @@ def get_cars():
     cars = cars_collection.find()
     return cars
 
+@router.get("/cars/{brand}")
+def get_car(brand: str):
+    car = cars_collection.find_one({"brand": brand})
+    if car:
+        return car
+    raise HTTPException(status_code=404, detail="Car not found")
+
 @router.post("/cars")
 def create_car(car: Car):
+    if cars_collection.find_one({"brand": car.brand, "model": car.model}):
+        raise HTTPException(status_code=400, detail="Car already exists")
+    
+  
     cars_collection.insert_one(car.model_dump())
     return car
+
